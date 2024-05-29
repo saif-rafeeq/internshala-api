ef0b2eeac9569e6eda169136e110d3d2564e56ba
saif-rafeeq <saifrafeeq121@gmail.com>
2024-05-29 23:30:40 +0530

    session

--- utils/SendToken.js
+++ utils/SendToken.js
@@ -1,27 +1,41 @@
 exports.sendToken = function (student, statusCode, res) {
     const token = student.getjwttoken()
-
-    const option = {
-        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+    const cookieExpireDays = parseInt(process.env.COOKIE_EXPIRE);
+    const expirationDate = new Date(Date.now() + cookieExpireDays * 24 * 60 * 60 * 1000);
+    // const option = {
+    //     expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+    //     httpOnly: true,
+    //     secure:true,
+    //     sameSite:"none"
+    // }
+    const options = {
+        expires: expirationDate,
         httpOnly: true,
-        secure:true,
-        sameSite:"none"
-    }
+        secure: true,
+        sameSite: "none"
+    };
 
-    res.status(statusCode).cookie("token",token,option).json({success:true, id:student._id,token})
+    res.status(statusCode).cookie("token",token,options).json({success:true, id:student._id,token})
 
 }
 
 exports.sendTokenEmploye = function (employe, statusCode, res) {
     const token = employe.getjwttoken()
-
-    const option = {
-        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+    const cookieExpireDays = parseInt(process.env.COOKIE_EXPIRE);
+    const expirationDate = new Date(Date.now() + cookieExpireDays * 24 * 60 * 60 * 1000);
+    // const option = {
+    //     expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+    //     httpOnly: true,
+    //     secure:true,
+    //     sameSite:"none"
+    // }
+    const options = {
+        expires: expirationDate,
         httpOnly: true,
-        secure:true,
-        sameSite:"none"
-    }
+        secure: true,
+        sameSite: "none"
+    };
 
-    res.status(statusCode).cookie("token",token,option).json({success:true, id:employe._id,token})
+    res.status(statusCode).cookie("token",token,options).json({success:true, id:employe._id,token})
 
 }
